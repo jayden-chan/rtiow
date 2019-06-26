4dd565a6c732274b95ba4a465935db4a504bfa7d
Jayden Chan <jaydencn7@gmail.com>
2019-06-26 15:20:54 -0700

    JC: Remove unnecessary second Newton's method

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,9 @@
+// TODO: This function should be replaced with RSQRT SIMD
 #[inline(always)]
 #[allow(clippy::all)] // Pointer casts in here cause warnings
 pub fn fast_inv_sqrt(x: f32) -> f32 {
     let i: u32 = unsafe { std::mem::transmute(x) };
-
     let j = 0x5F3759DF - (i >> 1);
-
     let y: f32 = unsafe { std::mem::transmute(j) };
-
-    // Two iterations of Newton's method for now
-    let y = y * (1.5 - 0.5 * x * y * y);
     y * (1.5 - 0.5 * x * y * y)
 }
