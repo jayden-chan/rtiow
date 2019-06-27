4f0a2e36fcca6bfe81599e38a5c562f834218203
Jayden Chan <jaydencn7@gmail.com>
2019-06-27 13:01:21 -0700

    JC: Image writing

---
+++ src/image.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub struct Pixel {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+pub fn gen_ppm(image: Vec<Vec<Pixel>>) {
+    let path = Path::new("image.ppm");
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => {
+            panic!("Couldn't create {}: {}", display, why.description())
+        }
+        Ok(file) => file,
+    };
+
+    let height = image.len();
+
+    assert!(height > 0);
+
+    let width = image[0].len();
+
+    let img_header = format!("P6\n{} {}\n255\n", width, height);
+    let mut img_buffer: Vec<u8> = Vec::from(img_header.as_bytes());
+
+    for (idx, row) in image.iter().enumerate() {
+        assert!(row.len() == width);
+
+        print!(
+            "Writing file... {}%\r",
+            ((idx as f32 / height as f32) * 100.0) as u8
+        );
+
+        for pixel in row {
+            img_buffer.push(pixel.r);
+            img_buffer.push(pixel.g);
+            img_buffer.push(pixel.b);
+        }
+    }
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(&img_buffer) {
+        Err(why) => {
+            panic!("\nCouldn't write to {}: {}", display, why.description())
+        }
+        Ok(_) => println!("\nImage written to {}", display),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+mod image;
 mod util;
 mod vector3;
 
+use image::{gen_ppm, Pixel};
 use vector3::Vector;
 
-fn main() {
-    let v = Vector::new(3.0, 3.0, 3.0);
-    let v_norm = v.normalize();
+const IMG_SIZE: usize = 4096;
 
-    println!("{:?}", v_norm);
+fn main() {
+    let mut image = Vec::new();
+
+    for h in 0..IMG_SIZE {
+        image.push(Vec::new());
+
+        print!(
+            "Rendering... {}%\r",
+            ((h as f32 / IMG_SIZE as f32) * 100.0) as u8
+        );
+
+        for w in 0..IMG_SIZE {
+            let c = ((h as f32 / IMG_SIZE as f32) * 255 as f32) as u8;
+            let c2 = ((w as f32 / IMG_SIZE as f32) * 255 as f32) as u8;
+            image[h].push(Pixel {
+                r: c,
+                g: 255 - c,
+                b: c2,
+            });
+        }
+    }
+
+    println!("\nFinished rendering.");
+    gen_ppm(image);
 }
