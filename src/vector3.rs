c355e474b0de0de8473f35da2dce70326ee00ccc
Jayden Chan <jaydencn7@gmail.com>
2019-06-28 09:06:00 -0700

    JC: Remove constructor for Vector struct

--- src/vector3.rs
+++ src/vector3.rs
@@ -2,9 +2,9 @@ use crate::util::fast_inv_sqrt;
 
 #[derive(Debug)]
 pub struct Vector {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vector {
@@ -19,10 +19,5 @@ impl Vector {
             y: self.y * mag_inv,
             z: self.z * mag_inv,
         }
-    }
-
-    /// Create a new vector
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Self { x, y, z }
     }
 }
