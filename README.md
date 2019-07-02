69acbc60be93f5245ec0183fa7f6026af0b09407
Jayden Chan <jaydencn7@gmail.com>
2019-07-01 18:58:11 -0700

    JC: Add render of spheres_2 scene

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Weekend"](http://www.realtimerendering.com/raytracing/Ray%20Tracing%20in%20a%20W
 
 ## Progress Pictures
 
+### Dielectrics: Hollow Glass Sphere - 2019-07-01
+- Scene: `scenes/spheres_2.json`
+- 1920 x 1080
+- 5000 samples per pixel
+- Machine: GCP n1-highcpu-96
+- Render time: 00:01:20
+
+![](/img/spheres_2.png "Hollow Dielectric Sphere")
+
 ### Lambertian & Metal spheres - 2019-07-01
 - Scene: `scenes/spheres.json`
 - 1920 x 1080
--- util.sh
+++ util.sh
@@ -5,12 +5,14 @@ case $1 in
         sudo apt update
         sudo apt install gcc htop
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        source $HOME/.cargo/env
+        mkdir out
         ;;
     connect)
-        ssh -i ~/.ssh/gcp_ssh jayden@$2
+        ssh -i ~/.ssh/gcp_ssh jayden@$GCP_VM_IP
         ;;
     download)
-        scp -i ~/.ssh/gcp_ssh jayden@$2:/home/jayden/raytracer/out/image.ppm ./out/image.ppm
+        scp -i ~/.ssh/gcp_ssh jayden@$GCP_VM_IP:/home/jayden/raytracer/out/image.ppm ./out/image.ppm
         ;;
     convert)
         convert out/image.ppm img/$2.png
